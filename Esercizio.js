c7bfd02a69a1fa7b874fa53975555e7a2601491b
Giulio Maria Facin <giulio.maria@gmail.com>
2018-05-06 13:04:05 +0200

    Fino ad esercizio 7

---
+++ Esercizio.js
@@ -0,0 +1,15 @@
+function mouseOver() {
+    alert("sei sopra!!");
+}
+
+function mouseOut() {
+    alert("sei uscito!!");
+}
+
+function cliccato() {
+    alert("Hai cliccato!!");
+}
+
+function contexmenu() {
+    alert("Tasto destro!!");
+}
